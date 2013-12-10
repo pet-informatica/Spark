5ff34b774c21b350581722f88c4735be2e7bdde1
unknown <lnpa@cin.ufpe.br>
2013-12-10 15:51:33 -0300

    Retirando sobrecarga de metodos

--- spark.dart
+++ spark.dart
@@ -3,4 +3,4 @@ library spark;
 part 'src/listener.dart';
 part 'src/emitter.dart';
 part 'src/message.dart';
-part 'src/gmc.dart'
+part 'src/gmc.dart';
--- src/emitter.dart
+++ src/emitter.dart
@@ -1,27 +1,23 @@
 part of spark;
 
 abstract class Emitter{
-	Map<String, List<Listener>> _eventMap;
+	Map<String, List<Listener>> _messageMap;
 	
 	Emitter(){
-		_eventMap = new Map<String, List<EventReceiver>>();
+		_messageMap = new Map<String, List<Listener>>();
 	}
 
-	void addListener(Listener l, String messageType){
+	void addListener(Listener l,[String messageType="__global__"]){
 		if(!this._messageMap.containsKey(messageType)){
 			this._messageMap[messageType] = new List<Listener>();
 		}
 		this._messageMap[messageType].add(l);
     }
-	
-	void addListener(Listener l){
-		this.addListener(l, "__global__");
-	}
 
 	void emitMessage(Message m){
 		String type = m.getMessageType();
-		List<Listener> specific = this._messageMap[type];
-		List<Listener> global = this._messageMap["__global__"];
+		List<Listener> specific = _messageMap[type];
+		List<Listener> global = _messageMap["__global__"];
 		if(specific){
 			for(int i = 0; i < specific.length; i++){
 				specific[i].onMessageReceived(m);
