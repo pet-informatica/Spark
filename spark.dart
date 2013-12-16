73261cb34d09fc6bde22cc83844cd3fbe1980294
Rafael Marinheiro <rfm3@cin.ufpe.br>
2013-12-16 12:34:55 -0300

    Removendo arquivos indesejados

--- spark.dart
+++
@@ -1,6 +0,0 @@
-library spark;
-
-part 'src/listener.dart';
-part 'src/emitter.dart';
-part 'src/message.dart';
-part 'src/gmc.dart';
--- src/listener.dart
+++
@@ -1,5 +0,0 @@
-part of spark;
-
-abstract class Listener{
-	void onMessageReceived(Message m);
-}
